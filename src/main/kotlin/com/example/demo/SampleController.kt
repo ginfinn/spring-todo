5feac60f1682e41a02fe05bb6df0115f27c467af
ginfin <gara_0100@mail.ru>
2020-03-24 20:40:22 +0300

    init

---
+++ src/main/kotlin/com/example/demo/SampleController.kt
@@ -0,0 +1,14 @@
+package com.example.demo
+
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.ResponseBody
+import org.springframework.web.bind.annotation.RestController
+
+@RestController
+class SampleController {
+    @RequestMapping("/")
+    @ResponseBody
+    fun home(): String {
+        return "Hello World!"
+    }
+}
